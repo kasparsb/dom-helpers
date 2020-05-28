2495f01a9cd32b5c765acd81af57cdc66631e11b
Kaspars Bulins <kasparsb@hotmail.com>
2020-05-28 13:13:39 +0000

    jsx createElements can handle Fragments and functions as elementName

--- src/jsx.js
+++ src/jsx.js
@@ -1,4 +1,5 @@
-import create from './create';
+import setAttributes from './setAttributes';
+import append from './append';
 
 /**
  * Helpers for using jsx syntax to create dom elements
@@ -6,7 +7,30 @@ import create from './create';
  */
 let jsx = {
     Fragment: 'fragment',
-    h: create
+
+    h: function (elementName, attributes, ...childs) {
+        let el;
+
+        if (elementName === this.Fragment) {
+            el = new DocumentFragment();
+        }
+        else if (typeof elementName == 'function') {
+            el = elementName(attributes);
+        }
+        else {
+            el = document.createElement(elementName);
+
+            if (attributes) {
+                setAttributes(el, attributes);
+            }
+        }
+
+        if (el) {
+            append(el, childs)
+        }
+
+        return el;
+    }
 }
 
 export default jsx
