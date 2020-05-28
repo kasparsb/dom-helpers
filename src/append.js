3c8ff56a181b43c9ff0e7098147c4b3842fc77bf
Kaspars Bulins <kasparsb@hotmail.com>
2020-05-28 11:37:29 +0000

    Add append ability to provider query selector string instead of dom element

--- src/append.js
+++ src/append.js
@@ -1,7 +1,19 @@
+import isArray from './isArray';
+import q from './q';
+
 function append(el, childs) {
+    // Ja el ir query selector string
+    if (typeof el === 'string') {
+        el = q(el);
+    }
+
+    if (!isArray(childs)) {
+        childs = [childs];
+    }
+
     for (let child of childs) {
 
-        if (Array.isArray(child)) {
+        if (isArray(child)) {
             this.appendChilds(el, child);
         }
         else if (typeof child === 'string') {
