04322628b5f66b8d81c95e0dd297b289c4318255
Kaspars Bulins <kasparsb@hotmail.com>
2022-07-08 15:14:39 +0000

    next atgriez tikai nodeType ELEMENT_NODE

--- src/next.js
+++ src/next.js
@@ -1,10 +1,26 @@
 import re from './re';
 
+
 /**
  * Return next node after passed node
  */
-export default function(el) {
+function next(el) {
+    if (!el) {
+        return null;
+    }
+
     el = re(el);
+
+    if (!el.nextSibling) {
+        return null;
+    }
+
+    // Ja next node nav ELEMENT_NODE, tad skip un atgriežam nākošo
+    if (el.nextSibling.nodeType !== Node.ELEMENT_NODE) {
+        return next(el.nextSibling);
+    }
 
     return el.nextSibling;
 }
+
+export default next;
