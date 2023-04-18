fc889bfbca2bf22051ca88df1d630c9e87078fdc
Kaspars Bulins <kasparsb@hotmail.com>
2023-04-18 21:30:26 +0300

    Partaisits bez form elements

--- src/clearFormData.js
+++ src/clearFormData.js
@@ -1,10 +1,11 @@
+import qa from './qa';
 import re from './re';
 import setValue from './setValue';
 
-export default function(form, data) {
+export default function(form) {
     form = re(form);
 
-    [...form.elements].forEach(field => setValue(field, null));
+    [...qa(form, 'input, select, textarea')].forEach(field => setValue(field, null));
 
     return form;
 }
