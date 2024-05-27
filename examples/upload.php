7381662a0847a2da7cd2f7e6014a5c7df1aa1d26
root <root@note2.my.domain>
2024-05-27 18:10:39 +0000

    Examples

--- examples/upload.php
+++ examples/upload.php
@@ -1,8 +1,24 @@
 <?php
 header('Content-Type: application/json; charset=utf-8');
 
-$data = $_GET;
-$data['upload_size'] = strlen(file_get_contents('php://input'));
+$fileName = filter_input(INPUT_GET, 'filename');
+$pi = pathinfo($fileName);
+
+
+move_uploaded_file($_FILES['file']['tmp_name'], '/var/www/_pad/dom-helpers/examples/uploads/image.'.$pi['extension']);
+
+
+
+// $fileContent = file_get_contents('php://input');
+
+// $data = $_GET;
+// $data['upload_size'] = strlen($fileContent);
+
+// file_put_contents('/var/www/_pad/dom-helpers/examples/uploads/image.'.$pi['extension'], $fileContent);
+
+
+
+
 
 echo json_encode($data);
 
