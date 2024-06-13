496fa76fd2f286443149f2c41f91123239f1f510
root <root@note2.my.domain>
2024-06-13 16:18:44 +0000

    form pielikta custom metode reset(), kas notīra forma datus

--- src/form.js
+++ src/form.js
@@ -4,6 +4,7 @@ import getFormData from './getFormData';
 import isInputCheckable from './isInputCheckable';
 import setValue from './setValue';
 import isArray from './isArray';
+import clearFormData from './clearFormData';
 /**
  * Interaktīvs form objekts
  * atgriež form lauka vērtību pēc name
@@ -17,10 +18,17 @@ export default function(formEl) {
 
     return new Proxy(getFormData(formEl), {
         get(target, fieldName, receiver) {
+
+            // Reset form fields
+            if (fieldName == 'reset') {
+                return function() {
+                    clearFormData(formEl);
+                }
+            }
+
             return target[fieldName];
         },
         set(obj, fieldName, value) {
-
             if (isArray(value)) {
                 let elements = qa(formEl, `[name="${fieldName}[]"]`);
                 for (let i = 0; i < elements.length; i++) {
