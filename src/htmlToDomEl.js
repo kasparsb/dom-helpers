aa987d45d63cb6e5c4983ad29386f92aae73d686
root <root@note2.my.domain>
2024-12-09 14:18:11 +0000

    htmlToDom atgriez pirmo isto node, ja padots text

--- src/htmlToDomEl.js
+++ src/htmlToDomEl.js
@@ -1,6 +1,21 @@
+import next from './next';
+
 export default function (html) {
     // Šis veido DOM document
     let newEl = (new DOMParser()).parseFromString(html, 'text/html')
-    // Ņemam tikai pirmo child no body
-    return newEl.body.firstChild;
+
+    /**
+     * Ņemam tikai pirmo child no body
+     * bet jāskatās, lai tas nav textNode
+     * ir jāmeklē pirmā īstā node
+     *
+     * TODO bet vai tomēr nevajag arī ar textNode korekti strādāt?
+     * problēma tā, ka ar textNode nevar strādāt kā ar domNode
+     */
+    let firstNode = newEl.body.firstChild;
+    if (firstNode.nodeType !== Node.ELEMENT_NODE) {
+        firstNode = next(firstNode);
+    }
+
+    return firstNode;
 }
